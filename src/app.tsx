003c77cd3a8b65781b9b17f11af6df93c91afdca
benjarongdev <benjarong_m@hotmail.com>
2023-08-09 14:34:59 +0700

    Update

---
+++ src/app.tsx
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client';
+import Home from './components/home';
+
+const container = document.getElementById('app');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(<Home/>);
